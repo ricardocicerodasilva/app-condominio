de7fcae1502208bf113f0a0d40d63b1504fd320a
ricardocicerodasilva <ricardocixero@gmail.com>
2024-10-18 22:35:35 -0300

    atualização

--- resources/views/homeadm.blade.php
+++
@@ -1,8 +0,0 @@
-@extends ('layout')
-@section('content')
- 
-<figure style="text-align: center;">
-<body style="background-image: url('assets/condominio.jpg'); background-size: cover; background-position: center; margin: 0; height: 100vh;">
-
-</figure>
-@endsection
